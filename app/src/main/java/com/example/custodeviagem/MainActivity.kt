d85e9a35446b949a87be1dd2183b836f3c4efae7
Raul Ward <raulwarddev@gmail.com>
2024-05-02 21:20:59 -0300

    :tada: initial commit!

---
+++ app/src/main/java/com/example/custodeviagem/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.custodeviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.custodeviagem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != ""
+                && binding.editAutonomy.text.toString().toFloat() != 0f)
+    }
+
+    private fun calculate() {
+
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+            "%.2f".format(totalValue)
+            val totalValueStr = "R$ ${"%.2f".format(totalValue)}"
+
+            binding.textResult.text = totalValueStr
+
+        } else {
+            Toast.makeText(this, R.string.toast_invalid_fields, Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+}
